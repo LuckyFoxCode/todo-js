3d90b81cf28a2862b7fb7da2a0c074863b3a76cf
LuckyFoxCode <sergei181989@gmail.com>
2025-03-26 17:43:01 +0200

    refactor: add JSDoc in App.js

--- js/components/App.js
+++ js/components/App.js
@@ -1,5 +1,9 @@
 import { createHeader, createFooter, createMainContent } from "./index.js";
 
+/**
+ * Creates the main application container with header, main content and footer
+ * @returns {HTMLElement} The complete application container
+ */
 export function createApp() {
   const app = document.createElement("div");
   app.classList.add("app");
